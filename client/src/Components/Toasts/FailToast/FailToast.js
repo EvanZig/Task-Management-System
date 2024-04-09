2c9fbd7d9bf9068f9851ebd79f1a996f02c53edb
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-04-10 00:06:47 +0300

    added custom toasts boilerplate

---
+++ client/src/Components/Toasts/FailToast/FailToast.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Toast } from 'react-bootstrap'
+import '../ToastStyles.scss'
+
+export default function FailToast() {
+  return (
+    <div>
+      Hello from Fail Toast
+      <Toast></Toast>
+    </div>
+  )
+}
---
+++ client/src/Components/Toasts/SuccessToast/SuccessToast.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Toast } from 'react-bootstrap'
+import '../ToastStyles.scss'
+
+export default function SuccessToast() {
+  return (
+    <div>
+      Hello from success toast
+      <Toast></Toast>
+    </div>
+  )
+}
