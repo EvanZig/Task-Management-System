5150ac42a5869997b829e206c549ef46b374c088
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-04-11 16:46:11 +0300

    improved structure

---
+++ client/src/Pages/TaskCreationWizzard.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function TaskCreationWizzard() {
+  return <div>Hello from TaskCreationWizzard</div>
+}
---
+++ client/src/services/API.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+// import headers from context which will be the jwt token.
+
+import { toast } from 'react-toastify' // import my custom toast component
+
+const useRequest = () => {
+  const baseURL = process.env.REACT_APP_SERVER_URL
+
+  const API = (headers) => {
+    const options = {
+      baseURL: baseURL,
+
+      headers: headers ? headers : undefined,
+    }
+
+    const instance = axios.create(options)
+
+    instance.interceptors.request.use(
+      (config) => {
+        return config
+      },
+
+      (error) => Promise.reject(error)
+    )
+
+    instance.interceptors.response.use(
+      (res) => {
+        if (res?.data?.message) {
+          toast.success(res.data.message)
+        }
+
+        return res
+      },
+
+      async (err) => {
+        const errorMessage = err?.response?.data?.error
+          ? err?.response?.data?.error
+          : err?.response?.data?.message
+          ? err?.response?.data?.message
+          : 'Unexpected error'
+
+        toast.error(errorMessage)
+
+        return Promise.reject(err)
+      }
+    )
+
+    return instance
+  }
+
+  return API
+}
+
+export default useRequest
