96343149827b2488cebad31b160c5b0749315f41
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-04-12 21:15:53 +0300

    added tailwind

--- client/src/Components/Tasks/TaskOverviewModal.js/TaskOverviewModal.js
+++ client/src/Components/Tasks/TaskOverviewModal.js/TaskOverviewModal.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Modal, Button } from 'react-bootstrap'
+import './TaskOverviewModal.scss'
 
 export default function TaskOverviewModal() {
-  return <div>Hello from TaskOverviewModal</div>
+  const [show, setShow] = useState(false)
+
+  const openModal = () => setShow(true)
+  const handleClose = () => setShow(false)
+
+  return (
+    <div>
+      <Button onClick={openModal} className="CreateTaskBtn hover:scale-105 ">
+        Create new task
+      </Button>
+      <Modal show={show} onHide={handleClose} className="modalContent">
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+        <div className="taskCreationFooter">
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="" className="">
+            Save changes
+          </Button>
+        </div>
+      </Modal>
+    </div>
+  )
 }
