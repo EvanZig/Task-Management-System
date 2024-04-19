4879afa813d82b67b9d173824a047511d4976238
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-04-19 15:44:33 +0300

    added task menu buttons

--- client/src/Components/TaskWrapper/TaskWrapper.js
+++ client/src/Components/TaskWrapper/TaskWrapper.js
@@ -4,11 +4,16 @@ import './TaskWrapper.scss'
 
 export default function MyTasks(props) {
   return (
-    <div className="rounded-xl flex flex-col w-80 min-h-40 taskWrapper m-2">
-      <div className="px-4 rounded-lg m-1 my-2 flex justify-center items-center taskWrapperTitle font-semibold text-lg">
-        {props.title}To do
+    <div
+      className="rounded-xl bg-[#a78c66] flex flex-col w-[22rem] min-h-40 taskWrapper m-2 border-2 border-black"
+      draggable
+    >
+      <div className="rounded-lg m-1 my-2 flex justify-center items-center">
+        <div className="px-4 font-semibold text-xl taskWrapperTitle text-black">
+          {props.title}To do
+        </div>
       </div>
-      <div className="bg-black">
+      <div className="mx-2 ">
         <Task />
       </div>
     </div>
--- client/src/Components/Tasks/Task.js
+++ client/src/Components/Tasks/Task.js
@@ -1,5 +1,62 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
-export default function Task() {
-  return <div>Hello from Task</div>
+export default function Task(props) {
+  const [isHovered, setIsHovered] = useState(false)
+  const [isOnMenu, setIsOnMenu] = useState(false)
+  const menuTimeoutRef = useRef(null)
+
+  const removeMenu = () => {
+    menuTimeoutRef.current = setTimeout(() => {
+      if (!isOnMenu) setIsHovered(false)
+    }, 100)
+  }
+
+  const handleMenuMouseEnter = () => {
+    setIsOnMenu(true)
+    clearTimeout(menuTimeoutRef.current)
+  }
+
+  const handleMenuMouseLeave = () => {
+    setIsOnMenu(false)
+    removeMenu()
+  }
+
+  return (
+    <div className="relative">
+      <div
+        className={`${
+          isHovered ? 'block' : 'hidden'
+        } absolute top-[-40px] right-[-10px]`}
+        onMouseEnter={handleMenuMouseEnter}
+        onMouseLeave={handleMenuMouseLeave}
+      >
+        <div className="taskActionMenu">
+          <button className="border-1 rounded-xl bg-slate-300 p-1 hover:cursor-pointer ml-2">
+            edit
+          </button>
+          <button className="border-1 rounded-xl bg-slate-300 p-1 ">
+            change priority
+          </button>
+          <button className="border-1 rounded-xl bg-slate-300 p-1 ">
+            delete
+          </button>
+        </div>
+      </div>
+      <div className="text-white rounded px-1 max-h-12 bg-emerald-900">
+        <div
+          className={
+            'w-full h-full resize-none text-white hover:cursor-pointer'
+          }
+          onMouseEnter={() => {
+            clearTimeout(menuTimeoutRef.current)
+            setIsHovered(true)
+          }}
+          onMouseLeave={removeMenu}
+          draggable
+        >
+          {props.title} Hello from task
+        </div>
+      </div>
+    </div>
+  )
 }
