5f251f8c879e13b39624b132887fb2735bdb5fc1
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-04-23 18:41:21 +0300

    added indicators for description buttons when active

--- client/src/Components/Tasks/TaskOverviewModal.js/TaskDescription.js
+++ client/src/Components/Tasks/TaskOverviewModal.js/TaskDescription.js
@@ -5,6 +5,7 @@ import { TbFileDescription } from 'react-icons/tb'
 export default function TaskDescription(props) {
   const [isBold, setIsBold] = useState(false)
   const [isUnderline, setIsUnderline] = useState(false)
+  const [isItalic, setIsItalic] = useState(false)
   const [isCode, setIsCode] = useState(false)
   const [isTyping, setIsTyping] = useState(false)
 
@@ -16,6 +17,11 @@ export default function TaskDescription(props) {
   const underlineClick = () => {
     console.log('underline clicked')
     setIsUnderline(!isUnderline)
+  }
+
+  const italicClick = () => {
+    console.log('italic clicked')
+    setIsItalic(!isItalic)
   }
 
   const codeClick = () => {
@@ -30,19 +36,35 @@ export default function TaskDescription(props) {
           Description <TbFileDescription size={18} className="ml-2" />
         </label>
         <div className="justify-end items-end px-1 pt-1 mr-1 flex mb-2">
-          <button className="hover:cursor-pointer mr-2 hover:scale-110">
+          <button
+            className={`hover:cursor-pointer mr-2 hover:scale-110 ${
+              isBold ? 'text-blue-400 scale-110' : ''
+            }`}
+          >
             <FaBold size={20} onClick={boldClick} />
           </button>
           <div className="border-l border-gray-400 h-6 mr-2"></div>
-          <button className="hover:cursor-pointer mr-2 hover:scale-110">
+          <button
+            className={`hover:cursor-pointer mr-2 hover:scale-110 ${
+              isUnderline ? 'text-blue-400 scale-110' : ''
+            }`}
+          >
             <FaUnderline size={19} onClick={underlineClick} />
           </button>
           <div className="border-l border-gray-400 h-6 mr-2"></div>
-          <button className="hover:cursor-pointer mr-2 hover:scale-110">
-            <FaItalic size={20} onClick={underlineClick} />
+          <button
+            className={`hover:cursor-pointer mr-2 hover:scale-110 ${
+              isItalic ? 'text-blue-400 scale-110' : ''
+            }`}
+          >
+            <FaItalic size={20} onClick={italicClick} />
           </button>
           <div className="border-l border-gray-400 h-6 mr-2"></div>
-          <button className="hover:cursor-pointer mr-1 hover:scale-110">
+          <button
+            className={`hover:cursor-pointer mr-2 hover:scale-110 ${
+              isCode ? 'text-blue-400 scale-110' : ''
+            }`}
+          >
             <FaCode size={20} onClick={codeClick} />
           </button>
         </div>
