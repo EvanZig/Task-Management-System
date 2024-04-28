77ff0d1cbdf921e6affcbae42a8ac9c155da1ee8
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-04-28 18:05:44 +0300

    added change template button

--- client/src/Components/Structure/Topbar/Topbar.js
+++ client/src/Components/Structure/Topbar/Topbar.js
@@ -1,5 +1,18 @@
 import React from 'react'
 
 export default function Topbar() {
-  return <div>Topbar Template</div>
+  const changeTemplate = () => {
+    console.log('Template Changed')
+  }
+  return (
+    <div>
+      Topbar Template
+      <button
+        className="border-2 border-black rounded-lg bg-slate-200 text-black hover:bg-slate-300"
+        onClick={changeTemplate}
+      >
+        Template
+      </button>
+    </div>
+  )
 }
