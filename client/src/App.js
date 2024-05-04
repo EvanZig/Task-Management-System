1c53dcc9f119e6955b5446568283cb7fdc3154b3
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-05-04 16:33:55 +0300

    addListButton boilerplate added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import AuthProvider from './Context/GlobalContext'
 import TaskOverviewModal from './Components/Tasks/TaskOverviewModal.js/TaskOverviewModal'
 import TaskWrapper from './Components/TaskWrapper/TaskWrapper'
 import Structure from './Components/Structure/Structure'
+import CreateList from './Components/List/CreateList'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Structure>
           <TaskOverviewModal />
           {/* <AppRoutes /> */}
+          <CreateList />
           <TaskWrapper />
         </Structure>
       </AuthProvider>
---
+++ client/src/Components/List/CreateList.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+export default function CreateList() {
+  const [addList, setAddList] = useState(false)
+
+  return (
+    <div>
+      <button
+        className="bg-slate-400 border-2 border-black p-2 rounded-md font-semibold px-4 hover:bg-slate-500"
+        onClick={() => setAddList(true)}
+      >
+        Add List
+      </button>
+      {addList && (
+        <div>
+          <input type="text" id="newListTitle" />
+        </div>
+      )}
+    </div>
+  )
+}
