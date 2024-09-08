12389614ba88ce74372806e9bd55188bd1724956
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-09-08 10:11:57 +0300

    allignment-fix

--- client/src/Components/Tasks/TaskOverviewModal.js/TaskOverviewModal.js
+++ client/src/Components/Tasks/TaskOverviewModal.js/TaskOverviewModal.js
@@ -54,32 +54,32 @@ export default function TaskOverviewModal() {
     <div>
       <button
         onClick={openModal}
-        className="text-black font-bold CreateTaskBtn py-2 px-3 rounded-sm flex items-center"
+        className='text-black font-bold CreateTaskBtn py-2 px-3 rounded-sm flex items-center'
       >
-        <FaPlus size={16} className="mr-1" />
+        <FaPlus size={16} className='mr-1' />
         Create New Task
       </button>
       {show && (
         <div
-          className="fixed inset-0 flex items-center justify-center z-50 text-white"
+          className='fixed inset-0 flex items-center justify-center z-50 text-white'
           onClick={handleOverlayClick}
         >
-          <div className="modal-overlay fixed inset-0 bg-black opacity-50"></div>
+          <div className='modal-overlay fixed inset-0 bg-black opacity-50'></div>
           <div
             ref={modalRef}
             onClick={handleModalClick}
-            className="modal-container modalContainer w-[51rem] p-6 rounded-lg z-50"
+            className='modal-container modalContainer w-[51rem] p-6 rounded-lg z-50'
           >
-            <div className="modal-content">
-              <div className="modal-header flex justify-between items-center mb-6">
+            <div className='modal-content'>
+              <div className='modal-header flex justify-between items-center mb-6'>
                 <div
-                  className="w-[90%] min-h-9 hover:cursor-text"
+                  className='w-[90%] min-h-9 hover:cursor-text'
                   onClick={titleEdit}
                 >
                   {isEditing ? (
                     <input
-                      type="text"
-                      className="text-black font-semibold rounded-sm shadow-md p-1 w-[100%] bg-gray-50 titleInput"
+                      type='text'
+                      className='text-black font-semibold rounded-sm shadow-md p-1 w-[100%] bg-gray-50 titleInput'
                       ref={titleRef}
                       onChange={formDataChange}
                       onKeyDown={(e) => {
@@ -88,23 +88,23 @@ export default function TaskOverviewModal() {
                       value={taskData.title}
                     />
                   ) : (
-                    <h1 className="text-lg font-semibold">
+                    <h1 className='text-lg font-semibold'>
                       {taskData.title ? taskData.title : 'New Task'}
                     </h1>
                   )}
                 </div>
                 <button
-                  className="text-white  hover:scale-125 hover:cursor-default"
+                  className='text-white  hover:scale-125 hover:cursor-default'
                   onClick={handleClose}
                 >
                   <RiCloseLine size={26} />
                 </button>
               </div>
-              <div className="modal-body">
+              <div className='modal-body'>
                 <TaskDescription formDataChange={formDataChange} />
                 <AttachFileButton />
-                <div className="flex items-center justify-between mt-3">
-                  <div className="flex h-11">
+                <div className='flex items-center justify-between mt-3'>
+                  <div className='flex h-11'>
                     <AssignToButton />
                     <TaskDueDate />
                   </div>
@@ -113,15 +113,15 @@ export default function TaskOverviewModal() {
                   </div>
                 </div>
               </div>
-              <div className="modal-footer flex justify-end mt-10 mb-1">
+              <div className='modal-footer flex justify-end mt-10 mb-1'>
                 <button
-                  className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-sm mr-2"
+                  className='bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-sm mr-2'
                   onClick={handleClose}
                 >
                   Exit
                 </button>
-                <button className="addTaskBtn text-black font-bold py-2 px-4 rounded-sm flex items-center">
-                  <BiBookAdd size={19} className="mr-1" /> Create Task
+                <button className='addTaskBtn text-black font-bold py-2 px-4 rounded-sm flex items-center'>
+                  <BiBookAdd size={19} className='mr-1' /> Create Task
                 </button>
               </div>
             </div>
