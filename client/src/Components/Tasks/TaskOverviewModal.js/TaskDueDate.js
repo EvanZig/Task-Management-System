547be405c9303b4baee0ac46c5fa9b523522c2be
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-09-09 22:06:57 +0300

    fixed styling

--- client/src/Components/Tasks/TaskOverviewModal.js/TaskDueDate.js
+++ client/src/Components/Tasks/TaskOverviewModal.js/TaskDueDate.js
@@ -15,30 +15,30 @@ export default function TaskDueDate() {
   }
 
   return (
-    <div className="ml-3 flex">
-      <span className="pt-4 font-semibold mx-3 text-lg">Due Date</span>
+    <div className='ml-3 flex'>
+      <span className='pt-4 font-semibold mx-3 text-lg'>Due Date</span>
       <div
-        className="w-32 rounded flex justify-between hover:cursor-pointer border-none outline-none dropdownBtn"
+        className='w-32 rounded flex justify-between hover:cursor-pointer border-none outline-none dropdownBtn'
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         {isHovered ? (
           <>
             <button
-              className=" w-1/3 items-center justify-center flex text-black bg-red-600 hover:scale-105 border-2 border-black"
+              className=' w-1/3 items-center justify-center flex text-black bg-red-600 hover:scale-105 border-2 border-black'
               onClick={handleDelete}
             >
               <FaRegTrashAlt size={18} />
             </button>
             <button
-              className="text-black w-2/3 bg-blue-400 items-center justify-center flex hover:scale-105 border-2 border-black"
+              className='text-black w-2/3 bg-blue-400 items-center justify-center flex hover:scale-105 border-2 border-black'
               onClick={handleDateChange}
             >
               <MdEditCalendar size={18} />
             </button>
           </>
         ) : (
-          <div className="text-black flex justify-center rounded items-center w-full bg-slate-400 border-2 border-black">
+          <div className='text-black flex justify-center rounded items-center w-full bg-slate-400 border-2 border-black'>
             {selectedDate ? selectedDate : '-- / -- / --'}
           </div>
         )}
