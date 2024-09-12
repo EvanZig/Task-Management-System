c61d12f31136f40050398d2a2491eee4228a04fc
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-09-12 19:01:20 +0300

    task wrapper u.i fix

--- client/src/Components/TaskWrapper/TaskWrapper.js
+++ client/src/Components/TaskWrapper/TaskWrapper.js
@@ -5,18 +5,18 @@ import './TaskWrapper.scss'
 export default function MyTasks(props) {
   return (
     <div
-      className="rounded-xl bg-[#a78c66] flex flex-col w-[22rem] min-h-40 taskWrapper m-2 border-2 border-black"
+      className='rounded-xl bg-[#a78c66] flex flex-col w-[22rem] min-h-40 taskWrapper m-2 border-2 border-black'
       draggable
     >
-      <div className="rounded-lg m-1 my-2 flex justify-center items-center">
-        <div className="px-4 font-semibold text-xl taskWrapperTitle text-black">
+      <div className='rounded-lg m-1 my-2 flex justify-center items-center'>
+        <div className='px-4 font-semibold text-xl taskWrapperTitle text-black'>
           {props.title}To do
         </div>
       </div>
-      <div className="mx-2 ">
-        <Task color="yellow" />
-        <Task color="green" />
-        <Task color="red" />
+      <div className='mx-2 '>
+        <Task color='yellow' />
+        <Task color='green' />
+        <Task color='red' />
       </div>
     </div>
   )
