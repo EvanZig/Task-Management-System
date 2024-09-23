77eabf8e0d027a9bf1a813586358441506d61045
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-09-23 23:13:16 +0300

    removed task context

--- client/src/Components/Tasks/TaskContext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-// define the state of the tasks(on track,completed,ready for test, completed)
-
-export default function TaskContext() {
-  return <div>Placeholder fot TaskContext</div>
-}
