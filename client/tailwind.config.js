84795375bcdbd7d232e03392ac708ea5d35b5b1d
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-11-19 23:14:16 +0200

    added darkBlue color to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: '#1E3A8A',
+      },
+    },
   },
   plugins: [],
 }
