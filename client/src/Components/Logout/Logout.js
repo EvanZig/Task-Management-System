73ee5dd1ceb8b8cda453925765b88277a75de89f
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-12-13 21:04:17 +0200

    added logout button ui

--- client/src/Components/Logout/Logout.js
+++ client/src/Components/Logout/Logout.js
@@ -7,7 +7,10 @@ export default function Logout() {
   }
 
   return (
-    <button className='bg-red-500' onClick={handleLogout}>
+    <button
+      className='bg-red-500 border-2 border-black px-4 py-2 text-white rounded-lg hover:bg-red-400'
+      onClick={handleLogout}
+    >
       Logout
     </button>
   )
