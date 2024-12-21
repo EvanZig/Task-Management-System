3519c0bc33c293446b1c0af483cc647fb2095719
Evangelos Zigkas <evangeloszigkas@gmail.com>
2024-12-21 16:03:07 +0200

    cleaned up notificaiton Dropdown component

--- client/src/Components/NotificationDropdown/NotificationDropdown.js
+++ client/src/Components/NotificationDropdown/NotificationDropdown.js
@@ -2,25 +2,31 @@ import React from 'react'
 import { Dropdown } from 'antd'
 import { IoMdNotificationsOutline } from 'react-icons/io'
 
-const items = [
+const menuItems = [
   {
-    label: <a href='https://www.aliyun.com'>1nd menu item</a>,
+    label: <a href='https://www.aliyun.com'>1st menu item</a>,
     key: '1',
   },
 ]
 
-export default function NotificationDropdown() {
+const NotificationDropdown = () => {
+  const handleClick = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div>
       <Dropdown
-        menu={{ items }}
+        menu={{ items: menuItems }}
         trigger={['click']}
         className='p-2 border-2 border-white rounded-lg mr-2 bg-orange-400 hover:scale-105'
       >
-        <button onClick={(e) => e.preventDefault()}>
+        <button onClick={handleClick} className='flex items-center'>
           <IoMdNotificationsOutline size={26} />
         </button>
       </Dropdown>
     </div>
   )
 }
+
+export default NotificationDropdown
