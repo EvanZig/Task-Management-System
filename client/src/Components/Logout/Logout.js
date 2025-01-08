9ac1559a2304505d3d531d306e6e7cf260c39c2d
Evangelos Zigkas <evangeloszigkas@gmail.com>
2025-01-08 14:55:55 +0200

    made logout button border yellow

--- client/src/Components/Logout/Logout.js
+++ client/src/Components/Logout/Logout.js
@@ -8,7 +8,7 @@ export default function Logout() {
 
   return (
     <button
-      className='bg-red-500 border-2 border-black px-4 py-2 text-white rounded-lg hover:bg-red-400'
+      className='bg-red-500 border-2 border-darkYellow px-4 py-2 text-white rounded-lg hover:bg-red-400'
       onClick={handleLogout}
     >
       Logout
