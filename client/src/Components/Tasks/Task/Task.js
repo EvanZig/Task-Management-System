9ec4c3acedc2dd536b025f4d5522818ddcd14157
Evangelos Zigkas <evangeloszigkas@gmail.com>
2025-02-05 23:13:42 +0200

    changed timeout to 75 ms

--- client/src/Components/Tasks/Task/Task.js
+++ client/src/Components/Tasks/Task/Task.js
@@ -11,7 +11,7 @@ export default function Task(props) {
   const removeMenu = () => {
     menuTimeoutRef.current = setTimeout(() => {
       setIsHovered(false)
-    }, 60)
+    }, 75)
   }
 
   const handleMenuMouseEnter = () => {
