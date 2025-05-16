cea8be25e6f04e689164bfaadda0e0d86bb9e43f
Evangelos Zigkas <evangeloszigkas@gmail.com>
2025-05-16 22:14:42 +0300

    added file name showing to attachFileButton component

--- client/src/Components/TaskButtons/AttachFileButton/AttachFileButton.js
+++ client/src/Components/TaskButtons/AttachFileButton/AttachFileButton.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AttachFileButton.scss'
 
 export default function AttachFileButton() {
+  const [selectedFiles, setSelectedFiles] = useState([])
+
+  const handleAttachFileChange = (e) => {
+    const files = Array.from(e.target.files)
+    setSelectedFiles(files)
+    files.forEach((file) => {
+      console.log(file.name)
+    })
+  }
+
   return (
     <div className='mb-3 flex mt-3'>
       <div className='w-[75%] text-sm'>
-        File Names.img, docuemnt.dc {' map them here somehow for hover effects'}
+        {selectedFiles.length > 0
+          ? selectedFiles.map((file, idx) => (
+              <span key={idx} className='mr-2'>
+                {file.name}
+              </span>
+            ))
+          : 'No files selected'}
       </div>
       <label
         htmlFor='formFileMultiple'
@@ -18,6 +34,7 @@ export default function AttachFileButton() {
         id='formFileMultiple'
         multiple
         className='fileInput mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+        onChange={handleAttachFileChange}
       />
     </div>
   )
